4fd8996fddc44f030b9ae2e279a7ec413b6bd714
Tarun516 <taruncv516@gmail.com>
2025-09-07 16:31:39 +0530

    feat: auth

--- apps/web/components/providers.tsx
+++ apps/web/components/providers.tsx
@@ -2,9 +2,20 @@
 
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { useAuth } from "@clerk/nextjs";
 
+if (!process.env.NEXT_PUBLIC_CONVEX_URL) {
+    throw new Error("Missing NEXT_PUBLIC_CONVEX_URL in your env");
+}
 export function Providers({ children }: { children: React.ReactNode }) {
-  const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL || "");
-  return <ConvexProvider client={convex}>{children}</ConvexProvider>;
+    const convex = new ConvexReactClient(
+        process.env.NEXT_PUBLIC_CONVEX_URL || "",
+    );
+    return (
+        <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+            {children}
+        </ConvexProviderWithClerk>
+    );
 }
